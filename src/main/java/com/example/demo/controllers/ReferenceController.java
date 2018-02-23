98421df7f06901d1713963d08667bd0d32666bfd
Yoolbin Um <yoolbin.um@gmail.com>
2018-02-23 00:50:30 -0500

    Created relationship between Expereience, Education, Reference, SKills

--- src/main/java/com/example/demo/controllers/ReferenceController.java
+++ src/main/java/com/example/demo/controllers/ReferenceController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
 
-import com.example.demo.backend.domains.Reference;
-import com.example.demo.backend.repositories.ReferenceRepository;
+import com.example.demo.backend.domains.Contact;
+import com.example.demo.backend.domains.User;
+import com.example.demo.backend.repositories.ContactRepository;
+import com.example.demo.backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,41 +19,50 @@ import javax.validation.Valid;
 @Controller
 public class ReferenceController {
     @Autowired
-    ReferenceRepository referenceRepository;
+    UserRepository userRepository;
+
+    @Autowired
+    ContactRepository contactRepository;
 
     final private String ReferenceDir = "domains/reference/";
     final private String ReferenceURL = "/reference";
 
     @RequestMapping(ReferenceURL)
-    public String ReferenceList(Model model) {
-        model.addAttribute("references", referenceRepository.findAll());
+    public String ReferenceList(Model model, Authentication auth) {
+        User user = userRepository.findUserByUsername(auth.getName());
+        model.addAttribute("references", user.getResume().getReference());
         return ReferenceDir + "list";
     }
 
     @GetMapping(ReferenceURL + "/add")
     public String ReferenceForm(Model model) {
-        model.addAttribute("reference", new Reference());
+        model.addAttribute("reference", new Contact());
         return ReferenceDir + "form";
     }
 
     @PostMapping(ReferenceURL + "/process")
-    public String processForm(@Valid Reference Reference, BindingResult result) {
+    public String processForm(@Valid Contact contact, BindingResult result,Authentication auth) {
         if (result.hasErrors()) {
             return ReferenceDir + "form";
         }
-        referenceRepository.save(Reference);
+        contactRepository.save(contact);
+        User user = userRepository.findUserByUsername(auth.getName());
+        user.getResume().addReference(contact);
+        userRepository.save(user);
         return "redirect:" + ReferenceURL;
     }
 
     @RequestMapping(ReferenceURL + "/update/{id}")
     public String updateEducation(@PathVariable("id") long id, Model model) {
-        model.addAttribute("reference", referenceRepository.findOne(id));
+        model.addAttribute("reference", contactRepository.findOne(id));
         return ReferenceDir + "form";
     }
 
     @RequestMapping(ReferenceURL + "/delete/{id}")
-    public String ReferenceDelete(@PathVariable("id") long id) {
-        referenceRepository.delete(id);
+    public String ReferenceDelete(@PathVariable("id") long id, Authentication auth) {
+        User user = userRepository.findUserByUsername(auth.getName());
+        user.getResume().removeReference(contactRepository.findOne(id));
+        userRepository.save(user);
         return "redirect:" + ReferenceURL;
     }
 }
